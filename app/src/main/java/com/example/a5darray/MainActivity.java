a97c09e5c8c4cef83eab1e1223f3f5af76a60481
aletaffo <alessandrotaffini@gmail.com>
2021-12-09 11:40:30 +0100

    Inizio Toast

--- app/src/main/java/com/example/a5darray/MainActivity.java
+++ app/src/main/java/com/example/a5darray/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.a5darray;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     String nazioni[]={"Italia","Germania","Spagna"};
@@ -16,7 +19,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listaStati = (ListView)findViewById(R.id.lisArray);
-        ArrayAdapter<String>aaStati = new ArrayAdapter<String>(this,R.layout.activity_main,nazioni);
+        ArrayAdapter<String>aaStati = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,nazioni);
         listaStati.setAdapter(aaStati);
+
+
+
+        Button btn = (Button)findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Hai premuto il tasto",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
